f328f1a5e4830ad511fc0dfca1463802a3b26abd
NakaMTC <naka1125@outlook.jp>
2024-12-15 13:56:01 +0900

    二重起動の禁止

--- MtcEast/Program.cs
+++ MtcEast/Program.cs
@@ -7,11 +7,26 @@ namespace MtcEast
         /// </summary>
         [STAThread]
         static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+        {            //---- ２重起動防止 ----//
+            string mutexName = "MultiTrainController";  // アプリケーションごとにユニークな名前を設定
+            bool createdNew;
+            using (Mutex mutex = new(true, mutexName, out createdNew))    // Mutexを作成
+            {
+                if (!createdNew)
+                {
+                    // 既にMutexが存在する場合、アプリケーションを終了
+
+                    MessageBox.Show("アプリケーションは既に起動しています。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+
+                // To customize application configuration such as set high DPI settings or default font,
+                // see https://aka.ms/applicationconfiguration.
+                ApplicationConfiguration.Initialize();
+                Application.Run(new Form1());
+
+                mutex.ReleaseMutex();   // アプリケーション終了時にMutexを解放
+            }
         }
     }
 }
